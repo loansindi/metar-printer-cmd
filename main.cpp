6d3d05f58ab4f0ef044f0f5ac717c3ff1efe9dfa
Derek Bever <derek.bever@gmail.com>
2018-05-21 09:45:22 -0500

    add platform detection

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 using namespace std;
 long response_code;
 QString city;
+QStringList cities;
 
 
 struct MemoryStruct {
@@ -43,93 +44,100 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 }
 
 void printReport() {
+    /*
     if(city.length() == 0)
     {
         return;
     }
-    CURL *curl_handle = nullptr;
-    CURLcode res;
+    */
+    for(int i=0; i<cities.length(); i++){
+        city = cities[i];
+        qDebug() << city;
+        CURL *curl_handle = nullptr;
+        CURLcode res;
 
-    struct MemoryStruct chunk;
+        struct MemoryStruct chunk;
 
-    chunk.memory = (char*)malloc(1);  /* will be grown as needed by the realloc above */
-    chunk.size = 0;    /* no data at this point */
+        chunk.memory = (char*)malloc(1);  /* will be grown as needed by the realloc above */
+        chunk.size = 0;    /* no data at this point */
 
-    curl_global_init(CURL_GLOBAL_ALL);
+        curl_global_init(CURL_GLOBAL_ALL);
 
-    /* init the curl session */
-      curl_handle = curl_easy_init();
-    ostringstream os;
-    os << "http://tgftp.nws.noaa.gov/data/observations/metar/decoded/" << city.toStdString() << ".TXT";
-    string s =os.str();
-    cout << s;
+        /* init the curl session */
+        curl_handle = curl_easy_init();
+        ostringstream os;
+        os << "http://tgftp.nws.noaa.gov/data/observations/metar/decoded/" << city.toStdString() << ".TXT";
+        string s =os.str();
+        cout << s;
 
-    /* specify URL to get */
-    curl_easy_setopt(curl_handle, CURLOPT_URL, s.c_str());
+        /* specify URL to get */
+        curl_easy_setopt(curl_handle, CURLOPT_URL, s.c_str());
 
-    /* send all data to this function  */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        /* send all data to this function  */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
-    /* we pass our 'chunk' struct to the callback function */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+        /* we pass our 'chunk' struct to the callback function */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
 
-    /* some servers don't like requests that are made without a user-agent
+        /* some servers don't like requests that are made without a user-agent
        field, so we provide one */
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
-    /* get it! */
-    res = curl_easy_perform(curl_handle);
+        /* get it! */
+        res = curl_easy_perform(curl_handle);
 
-    /* check for errors */
-    if(res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
-              //QCoreApplication::exit(1);
-    }
+        /* check for errors */
+        if(res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+            //QCoreApplication::exit(1);
+        }
 
 
-    else {
-      /*
+        else {
+            /*
        * Now, our chunk.memory points to a memory block that is chunk.size
        * bytes big and contains the remote file.
        *
        * Do something nice with it!
        */
-        curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
-        qDebug() << response_code;
+            curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
+            qDebug() << response_code;
 
 
-      printf("%lu bytes retrieved\n", (long)chunk.size);
+            printf("%lu bytes retrieved\n", (long)chunk.size);
+        }
+
+        /* cleanup curl stuff */
+        curl_easy_cleanup(curl_handle);
+        //  curl_handle.
+        if(response_code == 200) {
+            qDebug() << chunk.memory;
+            QString weather(chunk.memory);
+#ifdef Q_PROCESSOR_ARM
+
+            QPrinter printer;
+            //printer.setOutputFileName("weather");
+            QPainter painter;
+            painter.begin(&printer);
+
+            QFont font = painter.font();
+            font.setPixelSize(12);
+            painter.setFont(font);
+            const QRect rectangle = QRect(0, 0, 250, 600);
+            QRect boundingRect;
+            //if(printer.printerState() == 0) {
+            painter.drawText(rectangle, Qt::TextWordWrap, weather, &boundingRect);
+            painter.end();
+            //}
+#endif
+        }
+
+        free(chunk.memory);
+
+        /* we're done with libcurl, so clean it up */
+        curl_global_cleanup();
     }
-
-    /* cleanup curl stuff */
-    curl_easy_cleanup(curl_handle);
-  //  curl_handle.
-    if(response_code == 200) {
-        qDebug() << chunk.memory;
-        QString weather(chunk.memory);
-
-
-        QPrinter printer;
-        //printer.setOutputFileName("weather");
-        QPainter painter;
-        painter.begin(&printer);
-
-        QFont font = painter.font();
-        font.setPixelSize(12);
-        painter.setFont(font);
-        const QRect rectangle = QRect(0, 0, 250, 600);
-        QRect boundingRect;
-        //if(printer.printerState() == 0) {
-           painter.drawText(rectangle, Qt::TextWordWrap, weather, &boundingRect);
-           painter.end();
-        //}
-    }
-
-    free(chunk.memory);
-
-    /* we're done with libcurl, so clean it up */
-    curl_global_cleanup();
 
 
 }
@@ -145,18 +153,23 @@ static void button() {
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+#ifdef Q_PROCESSOR_ARM
     wiringPiSetup();
     wiringPiISR(9, INT_EDGE_FALLING, &button);
     pinMode(9, INPUT);
     pullUpDnControl(9, PUD_UP);
+#endif
     QCommandLineParser parser;
     parser.setApplicationDescription("Command-line weather printer");
     parser.addHelpOption();
-    parser.addPositionalArgument("city", QCoreApplication::translate("main", "ICAO code to print report for"));
-
+    parser.addPositionalArgument("cityList", QCoreApplication::translate("main", "ICAO code to print report for"));
     parser.process(a);
+    cities = parser.positionalArguments();
+#ifdef Q_PROCESSOR_X86
+    printReport();
+    exit(0);
+#endif
 
-    city = parser.positionalArguments()[0];
     //printReport();
     //QCoreApplication::exit()
     return a.exec();
